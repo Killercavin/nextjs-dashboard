502ea08af2bad42a7a6c98bd799ad3c98efdcebe
Cavin <cavinlarry001@gmail.com>
2024-09-22 14:04:32 +0300

    Initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return(
+        <p>Customers</p>
+    );
+}
+
+export default Page;
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,17 +1,12 @@
-import { lusitana } from '@/app/ui/fonts';
-import {
-  AtSymbolIcon,
-  KeyIcon,
-  ExclamationCircleIcon,
-} from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { quicksand } from '@/app/ui/fonts';
+import { ArrowRight, AtSign, KeySquare } from 'lucide-react';
 import { Button } from './button';
 
 export default function LoginForm() {
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+        <h1 className={`${quicksand.className} mb-3 text-2xl`}>
           Please log in to continue.
         </h1>
         <div className="w-full">
@@ -31,7 +26,7 @@ export default function LoginForm() {
                 placeholder="Enter your email address"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <AtSign className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
@@ -51,12 +46,12 @@ export default function LoginForm() {
                 required
                 minLength={6}
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <KeySquare className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
         <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          Log in <ArrowRight className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div className="flex h-8 items-end space-x-1">
           {/* Add form errors here */}
